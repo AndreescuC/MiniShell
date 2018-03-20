ebd117ad5ab3c307c2ab7da87bbf871cd39e81ff
AndreescuC <andreescuconstantin.ac95@gmail.com>
2018-03-20 14:12:45 -0700

    Moved redirecting logic into function, added redirecting revert for cd [25p]

--- src/Command/cmd.h
+++ src/Command/cmd.h
@@ -13,5 +13,6 @@
  * Parse and execute a command.
  */
 int parse_command(command_t *cmd, int level, command_t *father);
+static int do_redirects(simple_command_t *s, int *saved_stdout, int *saved_stdin, int *saved_stderr);
 
 #endif /* _CMD_H */
